c48a943538db33dad99c97d61063c761f6f72274
Juan Galeano <juansedev31@gmail.com>
2025-04-15 19:57:02 -0500

    Update score.py and scores.py

--- backend/models/score.py
+++ backend/models/score.py
@@ -1,22 +1,34 @@
-from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy import Column, Integer, ForeignKey, CheckConstraint
 from sqlalchemy.orm import relationship
-from db import Base 
+from db import Base
 
 class UserScore(Base):
     __tablename__ = 'user_scores'
 
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("users.id"))  
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+    # Columnas en ingles
     easy = Column(Integer, default=0)
     normal = Column(Integer, default=0)
     hard = Column(Integer, default=0)
     hardcore = Column(Integer, default=0)
 
-    # Asegúrate de que el 'back_populates' coincida con la relación en User
+    # Columnas en espaniol
+    facil = Column(Integer, default=0)
+    normal_2 = Column(Integer, default=0)
+    dificil = Column(Integer, default=0)
+    diablo = Column(Integer, default=0)
+
+    # Relación con el modelo User
     user = relationship("User", back_populates="user_scores")
 
     def __repr__(self):
-        return f"<UserScore(user_id={self.user_id}, easy={self.easy}, normal={self.normal}, hard={self.hard}, hardcore={self.hardcore})>"
+        return (
+            f"<UserScore(user_id={self.user_id}, easy={self.easy}, normal={self.normal}, "
+            f"hard={self.hard}, hardcore={self.hardcore}, facil={self.facil}, "
+            f"normal_2={self.normal_2}, dificil={self.dificil}, diablo={self.diablo})>"
+        )
 
     def to_dict(self):
         return {
@@ -25,5 +37,9 @@ class UserScore(Base):
             'easy': self.easy,
             'normal': self.normal,
             'hard': self.hard,
-            'hardcore': self.hardcore
+            'hardcore': self.hardcore,
+            'facil': self.facil,
+            'normal_2': self.normal_2,
+            'dificil': self.dificil,
+            'diablo': self.diablo
         }
